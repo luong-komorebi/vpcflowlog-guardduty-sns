b313cdee403cdcf2b0fd4fb05ce3c324f76745ec
secsandman <secsnamdna@gmail.com>
2022-02-05 14:39:32 -0600

    ${module.s3.bucket_arn}"

--- modules/common/s3/main.tf
+++ modules/common/s3/main.tf
@@ -132,7 +132,8 @@ data "aws_iam_policy_document" "bucket_policy" {
       effect  = "Deny"
       actions = ["s3:*"]
       resources = [
-        "arn:${data.aws_partition.current.partition}:s3:::${join("", aws_s3_bucket.default.*.id)}"
+        "${module.s3.bucket_arn}",
+        "${module.s3.bucket_arn}"/*"
       ]
 
 

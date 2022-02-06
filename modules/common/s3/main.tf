0e59031e795b3c30bc20818a511365bdf070b42e
secsandman <secsnamdna@gmail.com>
2022-02-05 22:31:28 -0600

    aws_s3_bucket.default.id

--- modules/common/s3/main.tf
+++ modules/common/s3/main.tf
@@ -90,7 +90,7 @@ data "aws_iam_policy_document" "bucket_policy" {
       sid       = "DenyIncorrectEncryptionHeader"
       effect    = "Deny"
       actions   = ["s3:PutObject"]
-      resources = ["aws_s3_bucket.default.*.id"]
+      resources = ["aws_s3_bucket.default.id"]
 
       principals {
         identifiers = ["*"]
@@ -112,7 +112,7 @@ data "aws_iam_policy_document" "bucket_policy" {
       sid       = "DenyUnEncryptedObjectUploads"
       effect    = "Deny"
       actions   = ["s3:PutObject"]
-      resources = ["aws_s3_bucket.default.*.id"]
+      resources = ["aws_s3_bucket.default.id"]
 
       principals {
         identifiers = ["*"]
@@ -134,7 +134,7 @@ data "aws_iam_policy_document" "bucket_policy" {
       sid     = "ForceSSLOnlyAccess"
       effect  = "Deny"
       actions = ["s3:*"]
-      resources = ["aws_s3_bucket.default.*.id"]
+      resources = ["aws_s3_bucket.default.id"]
 
       principals {
         identifiers = ["*"]
